bd732c049472a42990d620b5b19f7232f636b145
msdharani1 <msdharaniofficail@gmail.com>
2024-03-10 13:45:26 +0530

    Rating start added

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const firebaseConfig = {
     appId: "1:929505697693:web:bc72606fcf6470de11d974",
     measurementId: "G-WDFLZR7MQB"
   };
-  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
@@ -18,26 +17,28 @@ const firebaseConfig = {
   
   // Handle form submission
   document.getElementById('reviewForm').addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    // Get form values
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const review = document.getElementById('review').value;
+      event.preventDefault();
+      
+      // Get form values
+      const name = document.getElementById('name').value;
+      const phone = document.getElementById('phone').value;
+      const review = document.getElementById('review').value;
+      const rating = document.querySelector('input[name="rating"]:checked').value;
   
-    // Save review data to Firebase
-    database.ref('photoshoot_reviews').push({
-      name: name,
-      email: email,
-      review: review
-    })
-    .then(() => {
-      alert('Review submitted successfully!');
-      document.getElementById('reviewForm').reset(); // Clear form fields after submission
-    })
-    .catch((error) => {
-      console.error('Error submitting review:', error);
-      alert('An error occurred. Please try again later.');
-    });
+      // Save review data to Firebase
+      database.ref('photoshoot_reviews').push({
+          name: name,
+          phone: phone,
+          review: review,
+          rating: rating
+      })
+      .then(() => {
+          alert('Review submitted successfully!');
+          document.getElementById('reviewForm').reset(); // Clear form fields after submission
+      })
+      .catch((error) => {
+          console.error('Error submitting review:', error);
+          alert('An error occurred. Please try again later.');
+      });
   });
   
